fcacdedd4d1dab126cd22b29bd48fafa74a4e70e
Tom Hulihan <hulihan.tom159@gmail.com>
2015-02-11 11:06:58 -0500

    Ensure the user is logged in before modifying user state

--- app/auth/controllers/users.rb
+++ app/auth/controllers/users.rb
@@ -3,20 +3,24 @@ module Auth
     # This controller doesn't expose any routes, but is used as a base for the
     # rest of the application's controllers.
     class Users < Base
+      helpers { include Services::Users }
+
       post '/users/?' do
         status 201
-        Services::Users.create(parse_json(req_body)).to_json
+        create(parse_json(req_body)).to_json
       end
 
       put '/users/:id/?' do |id|
+        ensure_user_logged_in! id
         status 200
         attrs = parse_json(req_body).except('password_hash', 'password_salt')
-        Services::Users.update(id, attrs).to_json
+        update(id, attrs).to_json
       end
 
       delete '/users/:id/?' do |id|
+        ensure_user_logged_in! id
         status 204
-        Services::Users.destroy(id).to_json
+        destroy(id).to_json
       end
     end
   end
