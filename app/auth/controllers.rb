ed37dc88c1825ad68ced5c67f514297e49c01fe3
Tom Hulihan <hulihan.tom159@gmail.com>
2015-02-11 11:06:39 -0500

    Add a sessions controller
    This controller handles creating and expiring sessions

--- app/auth/controllers.rb
+++ app/auth/controllers.rb
@@ -2,6 +2,7 @@ module Auth
   # This module holds all of the web controllers.
   module Controllers
     autoload :Base, 'auth/controllers/base'
+    autoload :Sessions, 'auth/controllers/sessions'
     autoload :Users, 'auth/controllers/users'
   end
 end
