050980b84b583306463a91a97ccd8dd7a0db5d09
Tom Hulihan <hulihan.tom159@gmail.com>
2015-02-11 10:57:25 -0500

    Add a migration to add sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208172921) do
+ActiveRecord::Schema.define(version: 20150209023628) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "key",        null: false
+    t.datetime "expires_on", null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sessions", ["user_id"], name: "index_sessions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name",          null: false
